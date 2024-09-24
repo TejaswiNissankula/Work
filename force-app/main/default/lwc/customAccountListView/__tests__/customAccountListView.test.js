718092b5f59d72cc81afeec43c5b327c1259b157
Tejaswi Nissankula <nissankulatejaswi@gmail.com>
2024-09-24 11:30:44 -0700

    jest test fix for all lightning components

--- force-app/main/default/lwc/customAccountListView/__tests__/customAccountListView.test.js
+++ force-app/main/default/lwc/customAccountListView/__tests__/customAccountListView.test.js
@@ -1,4 +1,4 @@
-import { createElement } from 'lwc';
+import { createElement } from '@lwc/engine-dom';
 import CustomAccountListView from 'c/customAccountListView';
 
 describe('c-custom-account-list-view', () => {
